0ad9b1afb947486263ef8b522ed4388f84879f42
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-09-17 10:27:13 +0700

    first commit

---
+++ Biến_và_kiểu_dữ_liệu_đơn_giản/Hoc-python.py
@@ -0,0 +1,43 @@
+# a = 100
+# print(type(a))
+
+
+# a = 29.93884835
+# print('%.2f' % a)
+# print('{:.2f}' .format(a))
+
+
+# a = 3 + 5j
+# print(a.real)
+# print(a.imag)
+
+
+# a = "28tech"
+# b = "123"
+# a, b = b, a
+# print(a, b)
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+# print(id(a))
+# print(id(b))
+# print(a is b)
+# print(a is not b)
+
+
+# s = '28tech c++ python java '
+# print('28tech' in s)
+# print('28tekh' in s)
+# print('28tekh' not in s)
+
+
+# s = input()
+# s = int(input("Vui long nhap so nguyen:"))
+# print(s)
+
+# t = 33
+# if t == 31:
+#     print(31)
+# else:
+#     print("du lieu khong hop le")
+
