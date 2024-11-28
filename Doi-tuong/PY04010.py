a6cdea7a82494318576b74329f8484522cc51d62
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-11-28 23:00:08 +0700

    .

---
+++ Doi-tuong/PY04010.py
@@ -0,0 +1,15 @@
+class ThiSinh:
+    def __init__(self, name, birth, x1, x2, x3):
+        self.name = name
+        self.birth = birth
+        self.x1 = x1
+        self.x2 = x2
+        self.x3 = x3
+
+    def __str__(self):
+        return "{} {} {:.1f}".format(self.name, self.birth, self.x1 + self.x2 + self.x3)
+
+
+if __name__ == "__main__":
+    x = ThiSinh(input(), input(), float(input()), float(input()), float(input()))
+    print(x)
